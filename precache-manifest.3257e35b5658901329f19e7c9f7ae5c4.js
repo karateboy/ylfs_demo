5e6c31eb128bd3fcb841b016fb313b96cb64ee02
Aragorn <karateboy.tw@gmail.com>
2019-11-11 17:53:48 +0800

    sync up

---
+++ precache-manifest.3257e35b5658901329f19e7c9f7ae5c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd5c7faa83406d4703a",
+    "url": "/css/app.26d751cc.css"
+  },
+  {
+    "revision": "b0d246e3d8b6ff49fd53",
+    "url": "/css/chunk-vendors.7b59a46b.css"
+  },
+  {
+    "revision": "c459c7aacc8092771ea70ef4e1e2fd67",
+    "url": "/img/logo.c459c7aa.jpg"
+  },
+  {
+    "revision": "2a971794cfc7de8fdacf7b9f61ead0a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c604e91b7930a8454eb7",
+    "url": "/js/about.bfc572aa.js"
+  },
+  {
+    "revision": "efd5c7faa83406d4703a",
+    "url": "/js/app.b2bd7ad0.js"
+  },
+  {
+    "revision": "b0d246e3d8b6ff49fd53",
+    "url": "/js/chunk-vendors.edc904c8.js"
+  },
+  {
+    "revision": "84492fffaea267c39d5f562a18a8da33",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
